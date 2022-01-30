e24706d7abe654f5546df0c90cee20606368ce5f
mariamqco <mariamqco@gmail.com>
2022-01-30 10:57:59 -0500

    code format improvement

--- assets/js/drinks.js
+++ assets/js/drinks.js
@@ -24,39 +24,49 @@ beerSize.classList.add("volume-drink-1");
 alcohol.classList.add("alcohol-drink-1");
 ph.classList.add("ph-drink-1");
 
+fetch(`${BASE_API_URL}/v2/beers/4`)
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then((drink1) => {
+    drink1.forEach((drink1) => {
+      title.appendChild(
+        document.createTextNode(`${drink1.name}`),
+        subtitle.appendChild(
+          document.createTextNode(`${drink1.tagline}`),
+          age.appendChild(
+            document.createTextNode(`Fabricada desde: ${drink1.first_brewed}`),
+            img.setAttribute("src", `${drink1.image_url}`),
+            description.appendChild(
+              document.createTextNode(`${drink1.description}`),
+              beerSize.appendChild(
+                document.createTextNode(`${drink1.volume.value} litros`),
+                alcohol.appendChild(
+                  document.createTextNode(
+                    `Alcohol en volumen: ${drink1.abv} %`
+                  ),
+                  ph.appendChild(document.createTextNode(`Ph: ${drink1.ph}`))
+                )
+              )
+            )
+          )
+        )
+      );
+    });
 
-fetch(`${ BASE_API_URL }/v2/beers/4`)
-    .then((response) => {
-        console.log(response);
-        return response.json();
-    })
-    .then((drink1) => {
-        drink1.forEach((drink1) => {
-            title.appendChild(document.createTextNode(`${drink1.name}`),
-                subtitle.appendChild(document.createTextNode(`${drink1.tagline}`),
-                    age.appendChild(document.createTextNode(`Fabricada desde: ${drink1.first_brewed}`),
-                        img.setAttribute('src', `${drink1.image_url}`),
-                        description.appendChild(document.createTextNode(`${drink1.description}`),
-                            beerSize.appendChild(document.createTextNode(`${drink1.volume.value} litros`),
-                                alcohol.appendChild(document.createTextNode(`Alcohol en volumen: ${drink1.abv} %`),
-                                    ph.appendChild(document.createTextNode(`Ph: ${drink1.ph}`)))))))
-
-            );
-
-        });
-
-        container.appendChild(title);
-        container.appendChild(subtitle);
-        container.appendChild(age);
-        container.appendChild(spanDescription);
-        spanDescription.appendChild(img);
-        spanDescription.appendChild(description);
-        container.appendChild(spanElements);
-        spanElements.appendChild(beerSize);
-        spanElements.appendChild(alcohol);
-        spanElements.appendChild(ph);
-    })
-    .catch((error => console.log(new Error(error))));
+    container.appendChild(title);
+    container.appendChild(subtitle);
+    container.appendChild(age);
+    container.appendChild(spanDescription);
+    spanDescription.appendChild(img);
+    spanDescription.appendChild(description);
+    container.appendChild(spanElements);
+    spanElements.appendChild(beerSize);
+    spanElements.appendChild(alcohol);
+    spanElements.appendChild(ph);
+  })
+  .catch((error) => console.log(new Error(error)));
 
 //Drink-2
 const container2 = document.querySelector(".drink-2");
@@ -81,38 +91,49 @@ spanElements2.classList.add("elements-drink-2");
 beerSize2.classList.add("volume-drink-2");
 alcohol2.classList.add("alcohol-drink-2");
 ph2.classList.add("ph-drink-2");
-fetch(`${ BASE_API_URL }/v2/beers/12`)
-    .then((response2) => {
-        console.log(response2);
-        return response2.json();
-    })
-    .then((drink2) => {
-        drink2.forEach((drink2) => {
-            title2.appendChild(document.createTextNode(`${drink2.name}`),
-                subtitle2.appendChild(document.createTextNode(`${drink2.tagline}`),
-                    age2.appendChild(document.createTextNode(`Fabricada desde: ${drink2.first_brewed}`),
-                        img2.setAttribute('src', `${drink2.image_url}`),
-                        description2.appendChild(document.createTextNode(`${drink2.description}`),
-                            beerSize2.appendChild(document.createTextNode(`${drink2.volume.value} litros`),
-                                alcohol2.appendChild(document.createTextNode(`Alcohol en volumen: ${drink2.abv} %`),
-                                    ph2.appendChild(document.createTextNode(`Ph: ${drink2.ph}`)))))))
+fetch(`${BASE_API_URL}/v2/beers/12`)
+  .then((response2) => {
+    console.log(response2);
+    return response2.json();
+  })
+  .then((drink2) => {
+    drink2.forEach((drink2) => {
+      title2.appendChild(
+        document.createTextNode(`${drink2.name}`),
+        subtitle2.appendChild(
+          document.createTextNode(`${drink2.tagline}`),
+          age2.appendChild(
+            document.createTextNode(`Fabricada desde: ${drink2.first_brewed}`),
+            img2.setAttribute("src", `${drink2.image_url}`),
+            description2.appendChild(
+              document.createTextNode(`${drink2.description}`),
+              beerSize2.appendChild(
+                document.createTextNode(`${drink2.volume.value} litros`),
+                alcohol2.appendChild(
+                  document.createTextNode(
+                    `Alcohol en volumen: ${drink2.abv} %`
+                  ),
+                  ph2.appendChild(document.createTextNode(`Ph: ${drink2.ph}`))
+                )
+              )
+            )
+          )
+        )
+      );
+    });
 
-            );
-
-        });
-
-        container2.appendChild(title2);
-        container2.appendChild(subtitle2);
-        container2.appendChild(age2);
-        container2.appendChild(spanDescription2);
-        spanDescription2.appendChild(img2);
-        spanDescription2.appendChild(description2);
-        container2.appendChild(spanElements2);
-        spanElements2.appendChild(beerSize2);
-        spanElements2.appendChild(alcohol2);
-        spanElements2.appendChild(ph2);
-    })
-    .catch((error2 => console.log(new Error(error2))));
+    container2.appendChild(title2);
+    container2.appendChild(subtitle2);
+    container2.appendChild(age2);
+    container2.appendChild(spanDescription2);
+    spanDescription2.appendChild(img2);
+    spanDescription2.appendChild(description2);
+    container2.appendChild(spanElements2);
+    spanElements2.appendChild(beerSize2);
+    spanElements2.appendChild(alcohol2);
+    spanElements2.appendChild(ph2);
+  })
+  .catch((error2) => console.log(new Error(error2)));
 
 //Drink-3
 const container3 = document.querySelector(".drink-3");
@@ -137,35 +158,46 @@ spanElements3.classList.add("elements-drink-3");
 beerSize3.classList.add("volume-drink-3");
 alcohol3.classList.add("alcohol-drink-3");
 ph3.classList.add("ph-drink-3");
-fetch(`${ BASE_API_URL }/v2/beers/22`)
-    .then((response3) => {
-        console.log(response3);
-        return response3.json();
-    })
-    .then((drink3) => {
-        drink3.forEach((drink3) => {
-            title3.appendChild(document.createTextNode(`${drink3.name}`),
-                subtitle3.appendChild(document.createTextNode(`${drink3.tagline}`),
-                    age3.appendChild(document.createTextNode(`Fabricada desde: ${drink3.first_brewed}`),
-                        img3.setAttribute('src', `${drink3.image_url}`),
-                        description3.appendChild(document.createTextNode(`${drink3.description}`),
-                            beerSize3.appendChild(document.createTextNode(`${drink3.volume.value} litros`),
-                                alcohol3.appendChild(document.createTextNode(`Alcohol en volumen: ${drink3.abv} %`),
-                                    ph3.appendChild(document.createTextNode(`Ph: ${drink3.ph}`)))))))
+fetch(`${BASE_API_URL}/v2/beers/22`)
+  .then((response3) => {
+    console.log(response3);
+    return response3.json();
+  })
+  .then((drink3) => {
+    drink3.forEach((drink3) => {
+      title3.appendChild(
+        document.createTextNode(`${drink3.name}`),
+        subtitle3.appendChild(
+          document.createTextNode(`${drink3.tagline}`),
+          age3.appendChild(
+            document.createTextNode(`Fabricada desde: ${drink3.first_brewed}`),
+            img3.setAttribute("src", `${drink3.image_url}`),
+            description3.appendChild(
+              document.createTextNode(`${drink3.description}`),
+              beerSize3.appendChild(
+                document.createTextNode(`${drink3.volume.value} litros`),
+                alcohol3.appendChild(
+                  document.createTextNode(
+                    `Alcohol en volumen: ${drink3.abv} %`
+                  ),
+                  ph3.appendChild(document.createTextNode(`Ph: ${drink3.ph}`))
+                )
+              )
+            )
+          )
+        )
+      );
+    });
 
-            );
-
-        });
-
-        container3.appendChild(title3);
-        container3.appendChild(subtitle3);
-        container3.appendChild(age3);
-        container3.appendChild(spanDescription3);
-        spanDescription3.appendChild(img3);
-        spanDescription3.appendChild(description3);
-        container3.appendChild(spanElements3);
-        spanElements3.appendChild(beerSize3);
-        spanElements3.appendChild(alcohol3);
-        spanElements3.appendChild(ph3);
-    })
-    .catch((error3 => console.log(new Error(error3))));
+    container3.appendChild(title3);
+    container3.appendChild(subtitle3);
+    container3.appendChild(age3);
+    container3.appendChild(spanDescription3);
+    spanDescription3.appendChild(img3);
+    spanDescription3.appendChild(description3);
+    container3.appendChild(spanElements3);
+    spanElements3.appendChild(beerSize3);
+    spanElements3.appendChild(alcohol3);
+    spanElements3.appendChild(ph3);
+  })
+  .catch((error3) => console.log(new Error(error3)));
--- assets/js/international-food.js
+++ assets/js/international-food.js
@@ -1,101 +1,100 @@
-function createMealItem(mealId){
-    const BASE_API_URL = "https://www.themealdb.com/api/json/v1/1"
+function createMealItem(mealId) {
+  const BASE_API_URL = "https://www.themealdb.com/api/json/v1/1";
 
-    fetch(`${BASE_API_URL}/${mealId}`)
-    .then( (response) => response.json())
-    .then( (data) => {       
-        let mealFullData = data.meals[0];        
+  fetch(`${BASE_API_URL}/${mealId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let mealFullData = data.meals[0];
 
-        let meal = {
-            mealId: mealFullData.idMeal,
-            mealName: mealFullData.strMeal,
-            mealOrigin: mealFullData.strArea,
-            mealIngredients: getIngedients(mealFullData,"mealFullData"),
-            mealImage: mealFullData.strMealThumb,
+      let meal = {
+        mealId: mealFullData.idMeal,
+        mealName: mealFullData.strMeal,
+        mealOrigin: mealFullData.strArea,
+        mealIngredients: getIngedients(mealFullData, "mealFullData"),
+        mealImage: mealFullData.strMealThumb,
 
-            mealvideo: mealFullData.strYoutube              
-        }          
-        createMealItemHTML(meal)
-    })           
-    .catch( (error => console.log( new Error(error))));
+        mealvideo: mealFullData.strYoutube,
+      };
+      createMealItemHTML(meal);
+    })
+    .catch((error) => console.log(new Error(error)));
 }
 
-function getIngedients(mealFullData,mealFullDataStr){
-    let ingredientColletion = [];
-    for (let index = 1; index <= 20; index++){
-        let ingredient = eval(mealFullDataStr+"."+"strIngredient"+index);
-        if(ingredient !== null){
-            if (ingredient !== ""){
-                ingredientColletion.push(ingredient)
-            }
-        }            
+function getIngedients(mealFullData, mealFullDataStr) {
+  let ingredientColletion = [];
+  for (let index = 1; index <= 20; index++) {
+    let ingredient = eval(mealFullDataStr + "." + "strIngredient" + index);
+    if (ingredient !== null) {
+      if (ingredient !== "") {
+        ingredientColletion.push(ingredient);
+      }
     }
-    return ingredientColletion
+  }
+  return ingredientColletion;
 }
 
-function createMealItemHTML(mealObject){  
+function createMealItemHTML(mealObject) {
+  let categoryMain = document.querySelector("main");
 
-    let categoryMain = document.querySelector('main');
-    
-    let mealItemContainer = document.createElement('section');
-    mealItemContainer.className = 'meal-item-container';
+  let mealItemContainer = document.createElement("section");
+  mealItemContainer.className = "meal-item-container";
 
-    let mealName = document.createElement('h2');
-    mealName.className = 'meal-name';
-    mealName.textContent = mealObject.mealName;
+  let mealName = document.createElement("h2");
+  mealName.className = "meal-name";
+  mealName.textContent = mealObject.mealName;
 
-    let mealOrigin = document.createElement('p');
-    mealOrigin.className = 'meal-origin';
-    mealOrigin.textContent = mealObject.mealOrigin;
-    
-    let auxiliaryDiv = document.createElement('div');
-    auxiliaryDiv.className = 'auxiliary-div';
+  let mealOrigin = document.createElement("p");
+  mealOrigin.className = "meal-origin";
+  mealOrigin.textContent = mealObject.mealOrigin;
 
-    let mealImg = document.createElement('img');
-    mealImg.className = 'meal-img';
-    mealImg.src = mealObject.mealImage;
-    mealImg.alt =  "Picture of" + mealObject.mealName; 
-    
+  let auxiliaryDiv = document.createElement("div");
+  auxiliaryDiv.className = "auxiliary-div";
 
-    let ingredientContainer = document.createElement('div');
-    ingredientContainer.className = 'ingredient-container';
+  let mealImg = document.createElement("img");
+  mealImg.className = "meal-img";
+  mealImg.src = mealObject.mealImage;
+  mealImg.alt = "Picture of" + mealObject.mealName;
 
-    let ingredientTitle = document.createElement('p');
-    ingredientTitle.textContent = "INGREDIENTS";
+  let ingredientContainer = document.createElement("div");
+  ingredientContainer.className = "ingredient-container";
 
-    let ingredientList = document.createElement('ul');
+  let ingredientTitle = document.createElement("p");
+  ingredientTitle.textContent = "INGREDIENTS";
 
-    mealObject.mealIngredients.forEach(ingredient => {
-        let ingredientItem = document.createElement('li');
-        ingredientItem.textContent = ingredient;
-        ingredientList.appendChild(ingredientItem);      
-    });   
-    
-    let videoLinkTitle = document.createElement('h3');
-    videoLinkTitle.className = 'video-link-title';
-    videoLinkTitle.innerHTML = "Get an idea of what they will prepare for you <br> by visiting the video at the following link";  
-    
-    let videoLink = document.createElement('a');
-    videoLink.className = 'video-link';        
-    videoLink.textContent = "VIDEO"
-    videoLink.href = mealObject.mealvideo;
-    videoLink.target = '_blank';
-                    
-    categoryMain.appendChild(mealItemContainer);
-    mealItemContainer.appendChild(mealName);
-    mealItemContainer.appendChild(mealOrigin);
-    mealItemContainer.appendChild(auxiliaryDiv);
-    auxiliaryDiv.appendChild(mealImg);
-    auxiliaryDiv.appendChild(ingredientContainer);
-    ingredientContainer.appendChild(ingredientTitle);
-    ingredientContainer.appendChild(ingredientList);
-    mealItemContainer.appendChild(videoLinkTitle);     
-    mealItemContainer.appendChild(videoLink)    
+  let ingredientList = document.createElement("ul");
+
+  mealObject.mealIngredients.forEach((ingredient) => {
+    let ingredientItem = document.createElement("li");
+    ingredientItem.textContent = ingredient;
+    ingredientList.appendChild(ingredientItem);
+  });
+
+  let videoLinkTitle = document.createElement("h3");
+  videoLinkTitle.className = "video-link-title";
+  videoLinkTitle.innerHTML =
+    "Get an idea of what they will prepare for you <br> by visiting the video at the following link";
+
+  let videoLink = document.createElement("a");
+  videoLink.className = "video-link";
+  videoLink.textContent = "VIDEO";
+  videoLink.href = mealObject.mealvideo;
+  videoLink.target = "_blank";
+
+  categoryMain.appendChild(mealItemContainer);
+  mealItemContainer.appendChild(mealName);
+  mealItemContainer.appendChild(mealOrigin);
+  mealItemContainer.appendChild(auxiliaryDiv);
+  auxiliaryDiv.appendChild(mealImg);
+  auxiliaryDiv.appendChild(ingredientContainer);
+  ingredientContainer.appendChild(ingredientTitle);
+  ingredientContainer.appendChild(ingredientList);
+  mealItemContainer.appendChild(videoLinkTitle);
+  mealItemContainer.appendChild(videoLink);
 }
 
-let mealItemId13 ="lookup.php?i=52819";
-let mealItemId14 ="lookup.php?i=52820";
-let mealItemId15 ="lookup.php?i=53044";
+let mealItemId13 = "lookup.php?i=52819";
+let mealItemId14 = "lookup.php?i=52820";
+let mealItemId15 = "lookup.php?i=53044";
 createMealItem(mealItemId13);
 createMealItem(mealItemId14);
 createMealItem(mealItemId15);
